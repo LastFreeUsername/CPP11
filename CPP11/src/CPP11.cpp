515f6b96b3157720f660a77a048685ea5aa57b83
Gert <gert.kanter@gmail.com>
2013-06-11 22:28:14 +0300

    Initial commit

---
+++ CPP11/src/CPP11.cpp
@@ -0,0 +1,17 @@
+/*
+ * Gert playing around with C++11
+ */
+
+#include <cstdio>
+
+#include "CPP11.h"
+
+
+int main(int argc, char* args[])
+{
+
+
+	printf("[%d] %s\n", argc, args[0]);
+	return(0);
+
+}
